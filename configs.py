0643e4fd8c3f8a3836d75ad7c10c8fad945cee63
Reself <846074811@qq.com>
2021-12-10 18:23:45 +0800

    update

--- configs.py
+++ configs.py
@@ -9,9 +9,9 @@ def parse_args():
                         help='name of checkpoint model weight')
     parser.add_argument('--batch-size', default=36, type=int,
                         help='training batch size')
-    parser.add_argument('--epoch', default=120, type=int,
+    parser.add_argument('--epoch', default=240, type=int,
                         help='training epoch')
-    parser.add_argument('--lr', default=0.01, type=float,
+    parser.add_argument('--lr', default=0.02, type=float,
                         help='learning rate')
     parser.add_argument('--size', default=640, type=int,
                         help='image size')
